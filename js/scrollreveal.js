f868ca0f0c305ca1881165d7f38c5999c6be4a5a
Ling <lymcho@gmail.com>
2018-12-13 14:44:20 -0600

    update
    update

---
+++ js/scrollreveal.js
@@ -0,0 +1,11 @@
+
+// Changing the defaults
+window.sr = ScrollReveal({ scale: 0.9, easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)', reset: true });
+
+// Customizing a reveal set
+sr.reveal('.column', { duration: 500 });
+sr.reveal('.level', { duration: 500 });
+
+
+// //smoothscroll initializing settings
+// smoothScroll.init({speed:600,});
